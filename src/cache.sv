1eada5d2a74b851060ba8b7e2b87affa097da33e
Gray <gbruner@vols.utk.edu>
2020-11-15 18:54:28 -0500

    sram interface in progress

--- src/cache.sv
+++ src/cache.sv
@@ -7,7 +7,9 @@ module cache #(
 	parameter OFFSET_BITS=7, 
 	parameter INDEX_BITS=5, 
 	parameter TAG_BITS=20,
-	parameter N_POW=4
+	parameter N_POW=4,
+	parameter SRAM_ADDR_WIDTH=16, 
+	parameter SRAM_LATENCY=7
 	)(
 	//core to cache interface
 	input logic clk, rst, 
@@ -16,6 +18,13 @@ module cache #(
 	input logic [ADDR_WIDTH-1:0] addr, 
 	output logic cache_rdy, 
 	output logic [DATA_WIDTH-1:0] dout, 
+	
+	//cache to cache memory interface
+	input logic [7:0] cell_0_dout, cell_1_dout, cell_2_dout, cell_3_dout, 
+	output logic [7:0] cell_0_din, cell_1_din, cell_2_din, cell_3_din, 
+	output logic [SRAM_ADDR_WIDTH-1:0] cell_0_addr, cell_1_addr, cell_2_addr, cell_3_addr,
+	output logic cell_0_sense_en, cell_1_sense_en, cell_2_sense_en, cell_3_sense_en, 
+	output logic cell_0_wen, cell_1_wen, cell_2_wen, cell_3_wen,
 
 	//cache to mem interface 
 	input logic [DATA_WIDTH-1:0] mem_dout, 
@@ -52,6 +61,9 @@ module cache #(
 	
 	//Outputs from replacement scheme module
 	logic [N_POW-1:0] evict_way;
+	
+	logic [15:0] sram_latency_counter;
+	logic [1:0] sram_addr_lsb;
 
 	//Instantiate Address Translator 
 	address_translator a0(.*);
@@ -104,15 +116,87 @@ module cache #(
 							if (wen) cache_dirty[index_out][evict_way] = 1; 
 						end else if (cache_hit) begin
 							cache_age[index_out][hit_way] = 0; 
+							sram_addr_lsb <= addr[1:0]; 
+							case (addr[1:0])
+								2'b00: begin
+									cell_0_addr <= addr[SRAM_ADDR_WIDTH+1:2];
+									cell_1_addr <= addr[SRAM_ADDR_WIDTH+1:2];
+									cell_2_addr <= addr[SRAM_ADDR_WIDTH+1:2];
+									cell_3_addr <= addr[SRAM_ADDR_WIDTH+1:2];
+									if (wen) begin
+										cell_0_din <= din[7:0];
+										cell_1_din <= din[15:8];
+										cell_2_din <= din[23:16];
+										cell_3_din <= din[31:24];
+									end
+								end
+								2'b01: begin
+									cell_0_addr <= addr[SRAM_ADDR_WIDTH+1:2] + 1;
+									cell_1_addr <= addr[SRAM_ADDR_WIDTH+1:2];
+									cell_2_addr <= addr[SRAM_ADDR_WIDTH+1:2];
+									cell_3_addr <= addr[SRAM_ADDR_WIDTH+1:2];
+									if (wen) begin
+										cell_1_din <= din[7:0];
+										cell_2_din <= din[15:8];
+										cell_3_din <= din[23:16];
+										cell_0_din <= din[31:24];
+									end
+								end
+								2'b10: begin
+									cell_0_addr <= addr[SRAM_ADDR_WIDTH+1:2] + 1;
+									cell_1_addr <= addr[SRAM_ADDR_WIDTH+1:2] + 1;
+									cell_2_addr <= addr[SRAM_ADDR_WIDTH+1:2];
+									cell_3_addr <= addr[SRAM_ADDR_WIDTH+1:2];
+									if (wen) begin
+										cell_2_din <= din[7:0];
+										cell_3_din <= din[15:8];
+										cell_0_din <= din[23:16];
+										cell_1_din <= din[31:24];
+									end
+								end
+								2'b11: begin
+									cell_0_addr <= addr[SRAM_ADDR_WIDTH+1:2] + 1;
+									cell_1_addr <= addr[SRAM_ADDR_WIDTH+1:2] + 1;
+									cell_2_addr <= addr[SRAM_ADDR_WIDTH+1:2] + 1;
+									cell_3_addr <= addr[SRAM_ADDR_WIDTH+1:2];
+									if (wen) begin
+										cell_3_din <= din[7:0];
+										cell_0_din <= din[15:8];
+										cell_1_din <= din[23:16];
+										cell_2_din <= din[31:24];
+									end
+								end
+							endcase
+							cell_0_wen <= wen;
+							cell_1_wen <= wen;
+							cell_2_wen <= wen;
+							cell_3_wen <= wen;
+							cell_0_sense_en <= ren;
+							cell_1_sense_en <= ren;
+							cell_2_sense_en <= ren;
+							cell_3_sense_en <= ren;
+							state <= cache_rw;
 						end
 					end
-//					if (ren) begin
-//						read_flag <= 1;
-//						state <= cache_rw; 
-//					end else if (wen) begin
-//						write_flag <= 1;
-//						state <= cache_rw; 
-//					end
+				end
+				cache_rw: begin
+					if (sram_latency_counter >= SRAM_LATENCY) begin
+						sram_latency_counter <= 0; 
+						case (sram_addr_lsb) 
+							2'b00: begin
+								
+							end
+						endcase
+						
+						state <= idle;
+					end else begin
+						sram_latency_counter <= sram_latency_counter + 1;
+						cell_0_sense_en <= 1'b0;
+						cell_1_sense_en <= 1'b0;
+						cell_2_sense_en <= 1'b0;
+						cell_3_sense_en <= 1'b0;
+						cell_0_wen <= 1'b0; cell_1_wen <= 1'b0; cell_2_wen <= 1'b0; cell_3_wen <= 1'b0;
+					end 
 				end
 				default: begin
 					state <= idle;
